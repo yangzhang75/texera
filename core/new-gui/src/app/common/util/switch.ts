yangzhang75/texera

commit 0c6b8cd459bdae73907d149698771ccd170e1b92

Execution dashboard frontend (#2131)
This PR contains the frontend code for Execution Dashboard. For detailed
description, see #2130.

---------

Co-authored-by: MiuMiuMiue <yuanzhe2001@gmail.com>
Co-authored-by: Zhe Yuan <86388854+MiuMiuMiue@users.noreply.github.com>
Co-authored-by: Shengquan Ni <13672781+shengquan-ni@users.noreply.github.com>

core/new-gui/src/app/workspace/service/execute-workflow/execute-workflow.service.ts
@@ -113,7 +113,7 @@ export class ExecuteWorkflowService {
             } else {
               return { state: ExecutionState.Paused, currentTuples: {} };
             }
-          case ExecutionState.Aborted:
+          case ExecutionState.Failed:
           case ExecutionState.BreakpointTriggered:
             // for these 2 states, backend will send an additional message after this status event.
             return undefined;
@@ -152,11 +152,11 @@ export class ExecuteWorkflowService {
         Object.entries(event.generalErrors).forEach(entry => {
           errorMessages[entry[0]] = entry[1];
         });
-        return { state: ExecutionState.Aborted, errorMessages: errorMessages };
+        return { state: ExecutionState.Failed, errorMessages: errorMessages };
       // TODO: Merge WorkflowErrorEvent and ErrorEvent
       case "WorkflowExecutionErrorEvent":
         return {
-          state: ExecutionState.Aborted,
+          state: ExecutionState.Failed,
           errorMessages: { WorkflowExecutionError: event.message },
         };
       default:
@@ -169,7 +169,7 @@ export class ExecuteWorkflowService {
   }
 
   public getErrorMessages(): Readonly<Record<string, string>> | undefined {
-    if (this.currentState?.state === ExecutionState.Aborted) {
+    if (this.currentState?.state === ExecutionState.Failed) {
       return this.currentState.errorMessages;
     }
     return undefined;
@@ -354,7 +354,7 @@ export class ExecuteWorkflowService {
   private updateWorkflowActionLock(stateInfo: ExecutionStateInfo): void {
     switch (stateInfo.state) {
       case ExecutionState.Completed:
-      case ExecutionState.Aborted:
+      case ExecutionState.Failed:
       case ExecutionState.Uninitialized:
       case ExecutionState.BreakpointTriggered:
         this.workflowActionService.enableWorkflowModification();

core/new-gui/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { WorkspaceComponent } from "./workspace/component/workspace.component";
 import { HomeComponent } from "./home/component/home.component";
 import { AuthGuardService } from "./common/service/user/auth-guard.service";
 import { AdminUserComponent } from "./dashboard/admin/component/admin-user.component";
+import { AdminDashboardComponent } from "./dashboard/admin/component/admin-dashboard.component";
 import { AdminGuardService } from "./dashboard/admin/service/admin-guard.service";
 import { SearchComponent } from "./dashboard/user/component/search/search.component";
 /*
@@ -28,7 +29,6 @@ const routes: Routes = [
     canActivate: [AuthGuardService],
   },
 ];
-
 if (environment.userSystemEnabled) {
   /*
    *  The user dashboard is under path '/dashboard'
@@ -38,7 +38,6 @@ if (environment.userSystemEnabled) {
    *  The user project list is under path '/dashboard/project'
    *  The single user project is under path 'dashboard/project/{pid}'
    */
-
   routes.push({
     path: "dashboard",
     component: DashboardComponent,
@@ -69,6 +68,11 @@ if (environment.userSystemEnabled) {
         component: AdminUserComponent,
         canActivate: [AdminGuardService],
       },
+      {
+        path: "admin-dashboard",
+        component: AdminDashboardComponent,
+        canActivate: [AdminGuardService],
+      },
     ],
   });
 
@@ -77,13 +81,11 @@ if (environment.userSystemEnabled) {
     component: HomeComponent,
   });
 }
-
 // redirect all other paths to index.
 routes.push({
   path: "**",
   redirectTo: "",
 });
-
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
   exports: [RouterModule],

core/new-gui/src/app/app.module.ts
@@ -103,6 +103,7 @@ import { PresetWrapperComponent } from "./common/formly/preset-wrapper/preset-wr
 import { NzModalCommentBoxComponent } from "./workspace/component/workflow-editor/comment-box-modal/nz-modal-comment-box.component";
 import { NzCommentModule } from "ng-zorro-antd/comment";
 import { AdminUserComponent } from "./dashboard/admin/component/admin-user.component";
+import { AdminDashboardComponent } from "./dashboard/admin/component/admin-dashboard.component";
 import { NzPopconfirmModule } from "ng-zorro-antd/popconfirm";
 import { AdminGuardService } from "./dashboard/admin/service/admin-guard.service";
 import { ContextMenuComponent } from "./workspace/component/workflow-editor/context-menu/context-menu/context-menu.component";
@@ -146,6 +147,7 @@ registerLocaleData(en);
     OperatorLabelComponent,
     DashboardComponent,
     AdminUserComponent,
+    AdminDashboardComponent,
     UserIconComponent,
     UserAvatarComponent,
     LocalLoginComponent,

core/new-gui/src/app/common/type/execution.ts
@@ -1,0 +1,13 @@
+export interface Execution {
+  workflowName: string;
+  workflowId: number;
+  userName: string;
+  userId: number;
+  executionId: number;
+  executionStatus: string;
+  executionTime: number;
+  executionName: string;
+  startTime: number;
+  endTime: number;
+  access: boolean;
+}

core/new-gui/src/app/common/type/user.ts
@@ -1,3 +1,4 @@
+import { NumberValue } from "d3";
 import { Point } from "../../workspace/types/workflow-common.interface";
 
 /**

core/new-gui/src/app/dashboard/admin/component/admin-dashboard.component.html
@@ -1,0 +1,127 @@
+<nz-card
+  class="section-title"
+  style="background-color: lightcoral">
+  <h2
+    class="page-title"
+    style="color: white">
+    Executions
+  </h2>
+</nz-card>
+
+<nz-table
+  #basicTable
+  [nzData]="listOfExecutions">
+  <thead>
+    <tr>
+      <th
+        [nzSortFn]="sortByWorkflowName"
+        nzWidth="16%">
+        Workflow (ID)
+      </th>
+      <th
+        [nzSortFn]="sortByExecutionName"
+        nzWidth="16%">
+        Execution Name (ID)
+      </th>
+      <th
+        [nzSortFn]="sortByInitiator"
+        nzWidth="12%">
+        Initiator
+      </th>
+      <th
+        nzShowFilter
+        [nzFilters]="[
+          { text: 'READY', value: 'READY' },
+          { text: 'RUNNING', value: 'RUNNING'},
+          { text: 'PAUSED', value: 'PAUSED'},
+          { text: 'COMPLETED', value: 'COMPLETED'},
+          { text: 'FAILED', value: 'FAILED'},
+          { text: 'KILLED', value: 'KILLED'},
+          { text: 'JUST COMPLETED', value: 'JUST COMPLETED'}, 
+          { text: 'UNKNOWN', value: 'UNKNOWN'}]"
+        [nzFilterFn]="filterByStatus"
+        nzWidth="16%">
+        Status
+      </th>
+      <th nzWidth="10%">Time Used (hh:mm:ss)</th>
+      <th
+        [nzSortFn]="sortByCompletedTime"
+        nzWidth="15%">
+        End Time
+      </th>
+      <th nzWidth="15%">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr *ngFor="let execution of basicTable.data">
+      <td>
+        <div *ngIf="execution.access; else normalWorkflowName">
+          <a href="/workflow/{{execution.workflowId}}">
+            {{ maxStringLength(execution.workflowName, 16) }} ({{ execution.workflowId }})
+          </a>
+        </div>
+        <ng-template #normalWorkflowName>
+          <div>{{ maxStringLength(execution.workflowName, 16) }} ({{ execution.workflowId }})</div>
+        </ng-template>
+      </td>
+      <td>{{ maxStringLength(execution.executionName, 20) }} ({{ execution.executionId }})</td>
+      <td>{{ execution.userName }}</td>
+      <td [ngStyle]="{ 'color': getStatusColor(execution.executionStatus) }"><b>{{ execution.executionStatus }}</b></td>
+      <td>{{ convertSecondsToTime(execution.executionTime) }}</td>
+      <td>{{ convertTimeToTimestamp(execution.executionStatus, execution.endTime) }}</td>
+      <td>
+        <button
+          type="button"
+          (click)="killExecution(execution.workflowId)"
+          [disabled]="execution.executionStatus === 'COMPLETED' ||
+                        execution.executionStatus === 'JUST COMPLETED' ||
+                        execution.executionStatus === 'FAILED' ||
+                        execution.executionStatus === 'KILLED'"
+          nz-button
+          nz-tooltip="kill execution {{execution.executionName}} of workflow {{execution.workflowName}}"
+          type="button">
+          <i
+            nz-icon
+            nzTheme="outline"
+            nzType="stop"></i>
+        </button>
+        <button
+          type="button"
+          (click)="pauseExecution(execution.workflowId)"
+          [disabled]="execution.executionStatus !== 'RUNNING'"
+          nz-button
+          nz-tooltip="pause execution {{execution.executionName}} of workflow {{execution.workflowName}}"
+          type="button">
+          <i
+            nz-icon
+            nzTheme="outline"
+            nzType="pause"></i>
+        </button>
+        <button
+          type="button"
+          (click)="resumeExecution(execution.workflowId)"
+          [disabled]="execution.executionStatus !== 'PAUSED'"
+          nz-button
+          nz-tooltip="resume execution {{execution.executionName}} of workflow {{execution.workflowName}}"
+          type="button">
+          <i
+            nz-icon
+            nzTheme="outline"
+            nzType="redo"></i>
+        </button>
+        <button
+          (click)="clickToViewHistory(execution.workflowId)"
+          nz-button
+          nz-tooltip="previous execution of the workflow: {{
+                      execution.workflowName
+                    }}"
+          type="button">
+          <i
+            nz-icon
+            nzTheme="outline"
+            nzType="history"></i>
+        </button>
+      </td>
+    </tr>
+  </tbody>
+</nz-table>

core/new-gui/src/app/dashboard/admin/component/admin-dashboard.component.spec.ts
@@ -1,0 +1,29 @@
+import { ComponentFixture, inject, TestBed, waitForAsync } from "@angular/core/testing";
+import { AdminDashboardComponent } from "./admin-dashboard.component";
+import { AdminExecutionService } from "../service/admin-execution.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+
+describe("AdminDashboardComponent", () => {
+  let component: AdminDashboardComponent;
+  let fixture: ComponentFixture<AdminDashboardComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AdminDashboardComponent],
+      providers: [NgbModal, AdminExecutionService],
+      imports: [HttpClientTestingModule, NzDropDownModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", inject([HttpTestingController], () => {
+    expect(component).toBeTruthy();
+  }));
+});

core/new-gui/src/app/dashboard/admin/component/admin-dashboard.component.ts
@@ -1,0 +1,297 @@
+import { Component, OnInit } from "@angular/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { AdminExecutionService } from "../service/admin-execution.service";
+import { Execution } from "../../../common/type/execution";
+import { NzTableFilterFn, NzTableSortFn } from "ng-zorro-antd/table";
+
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbdModalWorkflowExecutionsComponent } from "../../user/component/user-workflow/ngbd-modal-workflow-executions/ngbd-modal-workflow-executions.component";
+import { Workflow } from "../../../common/type/workflow";
+import { WorkflowWebsocketService } from "src/app/workspace/service/workflow-websocket/workflow-websocket.service";
+
+@UntilDestroy()
+@Component({
+  templateUrl: "./admin-dashboard.component.html",
+  styleUrls: ["./admin-dashboard.component.scss"],
+})
+export class AdminDashboardComponent implements OnInit {
+  Executions: ReadonlyArray<Execution> = [];
+  workflowsCount: number = 0;
+  usersCount: number = 0;
+  listOfExecutions = [...this.Executions];
+  workflows: Array<Workflow> = [];
+  executionMap: Map<number, Execution> = new Map();
+
+  // Set up an interval to periodically fetch and update execution data.
+  // This interval function fetches the latest execution list and checks for updates.
+  // If some execution's data has changed, it triggers a component refresh.
+  // The interval runs every 1 second (1000 milliseconds).
+  intervals = setInterval(() => {
+    this.adminExecutionService
+      .getExecutionList()
+      .pipe(untilDestroyed(this))
+      .subscribe(executionList => {
+        this.listOfExecutions.forEach((oldExecution, index) => {
+          const updatedExecution = executionList.find(execution => execution.executionId === oldExecution.executionId);
+          if (updatedExecution && this.dataCheck(this.listOfExecutions[index], updatedExecution)) {
+            this.ngOnInit();
+          } else if (!updatedExecution) {
+            this.ngOnInit();
+
+            // this if statement checks whether the workflow has no executions or the workflow has been deleted.
+            let check_execution = this.executionMap.get(oldExecution.workflowId);
+            if (check_execution && check_execution.executionId === oldExecution.executionId) {
+              this.executionMap.delete(oldExecution.workflowId);
+              this.listOfExecutions = [...this.executionMap.values()];
+            }
+          }
+        });
+
+        executionList.forEach(execution => {
+          if (this.executionMap.has(execution.workflowId)) {
+            let tempExecution = this.executionMap.get(execution.workflowId);
+            if (tempExecution) {
+              if (tempExecution.executionId < execution.executionId) {
+                this.ngOnInit();
+              }
+            }
+          } else if (!this.executionMap.has(execution.workflowId)) {
+            this.ngOnInit();
+          }
+        });
+        this.updateTimeDifferences();
+      });
+  }, 1000); // 1 second interval
+
+  constructor(private adminExecutionService: AdminExecutionService, private modalService: NgbModal) {}
+
+  ngOnInit() {
+    this.adminExecutionService
+      .getExecutionList()
+      .pipe(untilDestroyed(this))
+      .subscribe(executionList => {
+        this.Executions = executionList;
+        this.listOfExecutions = [];
+        this.reset();
+        this.workflowsCount = this.listOfExecutions.length;
+      });
+  }
+
+  maxStringLength(input: string, length: number): string {
+    if (input.length > length) {
+      return input.substring(0, length) + " . . . ";
+    }
+    return input;
+  }
+
+  dataCheck(oldExecution: Execution, newExecution: Execution): boolean {
+    // Get the current time in seconds.
+    const currentTime = Date.now() / 1000;
+    // Check if the execution needed to be updated
+    if (oldExecution.executionStatus === "JUST COMPLETED" && currentTime - newExecution.endTime / 1000 <= 5) {
+      return false;
+    } else if (oldExecution.executionStatus != newExecution.executionStatus) {
+      return true;
+    } else if (oldExecution.executionName != newExecution.executionName) {
+      return true;
+    } else if (oldExecution.workflowName != newExecution.workflowName) {
+      return true;
+    }
+    return false;
+  }
+
+  initWorkflows() {
+    for (let i = 0; i < this.listOfExecutions.length; i++) {
+      const execution = this.listOfExecutions[i];
+      let tempWorkflow: Workflow = {
+        content: { operators: [], operatorPositions: {}, links: [], groups: [], breakpoints: {}, commentBoxes: [] },
+        name: execution.workflowName,
+        wid: execution.workflowId,
+        description: "",
+        creationTime: 0,
+        lastModifiedTime: 0,
+        readonly: false,
+      };
+
+      this.workflows.push(tempWorkflow);
+    }
+  }
+
+  filterExecutions() {
+    for (let i = 0; i < this.Executions.length; i++) {
+      const execution = this.Executions[i];
+      this.executionMap.set(execution.workflowId, execution);
+    }
+    this.listOfExecutions = [...this.executionMap.values()];
+  }
+
+  reset() {
+    this.filterExecutions();
+    this.initWorkflows();
+
+    this.specifyCompletedStatus();
+    this.updateTimeDifferences();
+  }
+
+  /**
+   * Update the execution status of workflows in the list based on their completion time.
+   * If a workflow was completed within the last 5 seconds, it is updated to "JUST COMPLETED."
+   * If a workflow was completed more than 5 seconds, it is updated back to "COMPLETED."
+   */
+  specifyCompletedStatus() {
+    const currentTime = Date.now() / 1000;
+    this.listOfExecutions.forEach((workflow, index) => {
+      if (workflow.executionStatus === "COMPLETED" && currentTime - workflow.endTime / 1000 <= 5) {
+        this.listOfExecutions[index].executionStatus = "JUST COMPLETED";
+      } else if (workflow.executionStatus === "JUST COMPLETED" && currentTime - workflow.endTime / 1000 > 5) {
+        this.listOfExecutions[index].executionStatus = "COMPLETED";
+      }
+    });
+  }
+
+  /**
+   * if status are "RUNNING", "READY", or "PAUSED", the time used would constantly increase.
+   * if status are not list above, there would be a final time used.
+   */
+  calculateTime(LastUpdateTime: number, StartTime: number, executionStatus: string, name: string): number {
+    if (executionStatus === "RUNNING" || executionStatus === "READY" || executionStatus === "PAUSED") {
+      const currentTime = Date.now() / 1000; // Convert to seconds
+      const currentTimeInteger = Math.floor(currentTime);
+      return currentTimeInteger - StartTime / 1000;
+    } else {
+      return (LastUpdateTime - StartTime) / 1000;
+    }
+  }
+
+  /**
+   * Update the execution time differences for each execution in the list of executions.
+   * This function calculates and assigns the elapsed time for each execution.
+   */
+  updateTimeDifferences() {
+    this.listOfExecutions.forEach(workflow => {
+      workflow.executionTime = this.calculateTime(
+        workflow.endTime,
+        workflow.startTime,
+        workflow.executionStatus,
+        workflow.workflowName
+      );
+    });
+  }
+
+  /**
+   * Determine and return the color associated with a given execution status.
+   */
+  getStatusColor(status: string): string {
+    switch (status) {
+      case "READY":
+        return "lightgreen";
+      case "RUNNING":
+        return "orange";
+      case "PAUSED":
+        return "purple";
+      case "FAILED":
+        return "gray";
+      case "JUST COMPLETED":
+        return "blue";
+      case "COMPLETED":
+        return "green";
+      case "KILLED":
+        return "red";
+      default:
+        return "black";
+    }
+  }
+
+  /**
+   * Convert a numeric timestamp to a human-readable time string.
+   */
+  convertTimeToTimestamp(executionStatus: string, timeValue: number): string {
+    const date = new Date(timeValue);
+    const formattedTime = date.toLocaleString("en-US", { timeZoneName: "short" });
+    return formattedTime;
+  }
+
+  /**
+   * Convert a total number of seconds into a formatted time string (HH:MM:SS).
+   */
+  convertSecondsToTime(seconds: number): string {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedTime = `${this.padZero(hours)}:${this.padZero(minutes)}:${this.padZero(remainingSeconds)}`;
+    return formattedTime;
+  }
+
+  /**
+   * Pad a number with a leading zero if it is a single digit.
+   */
+  padZero(value: number): string {
+    return value.toString().padStart(2, "0");
+  }
+
+  filterByStatus: NzTableFilterFn<Execution> = function (list: string[], execution: Execution) {
+    return list.some(function (executionStatus) {
+      return execution.executionStatus.indexOf(executionStatus) !== -1;
+    });
+  };
+
+  clickToViewHistory(workflowId: number) {
+    const modalRef = this.modalService.open(NgbdModalWorkflowExecutionsComponent, {
+      size: "xl",
+      modalDialogClass: "modal-dialog-centered",
+    });
+
+    for (let i = 0; i < this.workflows.length; i++) {
+      const workflow = this.workflows[i];
+      if (workflow.wid == workflowId) {
+        let currentWorkflow: Workflow = workflow;
+        modalRef.componentInstance.workflow = currentWorkflow;
+        modalRef.componentInstance.workflowName = currentWorkflow.name;
+
+        break;
+      }
+    }
+  }
+
+  /**
+   Due to the Async nature when setting up the websocket, the socket would be closed before the connection is established.
+   Therefore, commenting the code to ensure the connections is established and request has been sent.
+   */
+  killExecution(wid: number) {
+    let socket = new WorkflowWebsocketService();
+    socket.openWebsocket(wid);
+    socket.send("WorkflowKillRequest", {});
+    // socket.closeWebsocket();
+  }
+
+  /**
+   Due to the Async nature when setting up the websocket, the socket would be closed before the connection is established.
+   Therefore, commenting the code to ensure the connections is established and request has been sent.
+   */
+  pauseExecution(wid: number) {
+    let socket = new WorkflowWebsocketService();
+    socket.openWebsocket(wid);
+    socket.send("WorkflowPauseRequest", {});
+    // socket.closeWebsocket();
+  }
+
+  /**
+   Due to the Async nature when setting up the websocket, the socket would be closed before the connection is established.
+   Therefore, commenting the code to ensure the connections is established and request has been sent.
+   */
+  resumeExecution(wid: number) {
+    let socket = new WorkflowWebsocketService();
+    socket.openWebsocket(wid);
+    socket.send("WorkflowResumeRequest", {});
+    // socket.closeWebsocket();
+  }
+
+  public sortByWorkflowName: NzTableSortFn<Execution> = (a: Execution, b: Execution) =>
+    (b.workflowName || "").localeCompare(a.workflowName);
+  public sortByExecutionName: NzTableSortFn<Execution> = (a: Execution, b: Execution) =>
+    (b.executionName || "").localeCompare(a.executionName);
+  public sortByCompletedTime: NzTableSortFn<Execution> = (a: Execution, b: Execution) => b.endTime - a.endTime;
+  public sortByInitiator: NzTableSortFn<Execution> = (a: Execution, b: Execution) =>
+    (b.userName || "").localeCompare(a.userName);
+}

commit 8fae0fd1602de9c3a3721b9cda907f8b61a9631b

Fix the issue that a killed workflow is not modifiable (#2205)
This PR fixes #2185. The breaking change was introduced in #2131, where
it adds a new workflow state `Killed`. The `Killed` state did not enable
the workflow to be modified.

core/new-gui/src/app/common/util/switch.ts
@@ -1,0 +1,3 @@
+export function exhaustiveGuard(_value: never): never {
+  throw new Error(`ERROR! Reached forbidden guard function with unexpected value: ${JSON.stringify(_value)}`);
+}

core/new-gui/src/app/workspace/service/execute-workflow/execute-workflow.service.ts
@@ -19,6 +19,7 @@ import { PAGINATION_INFO_STORAGE_KEY, ResultPaginationInfo } from "../../types/r
 import { sessionGetObject, sessionSetObject } from "../../../common/util/storage";
 import { Version as version } from "src/environments/version";
 import { NotificationService } from "src/app/common/service/notification/notification.service";
+import { exhaustiveGuard } from "../../../common/util/switch";
 
 // TODO: change this declaration
 export const FORM_DEBOUNCE_TIME_MS = 150;
@@ -357,6 +358,7 @@ export class ExecuteWorkflowService {
       case ExecutionState.Failed:
       case ExecutionState.Uninitialized:
       case ExecutionState.BreakpointTriggered:
+      case ExecutionState.Killed:
         this.workflowActionService.enableWorkflowModification();
         return;
       case ExecutionState.Paused:
@@ -368,7 +370,7 @@ export class ExecuteWorkflowService {
         this.workflowActionService.disableWorkflowModification();
         return;
       default:
-        return;
+        return exhaustiveGuard(stateInfo);
     }
   }
 
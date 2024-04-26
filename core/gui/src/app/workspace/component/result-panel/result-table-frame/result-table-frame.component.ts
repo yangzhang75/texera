yangzhang75/texera

commit 879be2d6048636dd12b14e571557db69d9e4dfe5

Making Result Panel Draggable, Resizable, and Collapsible (#2604)
This PR makes the Result Panel draggable, resizable, and also
colllapsible.

While Resizing the Result Panel, the nz-table inside the panel will also
be resizing, adjusting the number of rows as the panel size changed.

The location and status of the Result Panel will be memorized after
refreshing the page.



https://github.com/Texera/texera/assets/86388854/4814abfb-db96-4663-af08-dfcc3163a122

---------

Co-authored-by: mengw15 <125719918+mengw15@users.noreply.github.com>
Co-authored-by: aglinxinyuan <xinyual3@uci.edu>

core/gui/src/app/workspace/component/result-panel/result-table-frame/result-table-frame.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/cor
 import { NzModalRef, NzModalService } from "ng-zorro-antd/modal";
 import { NzTableQueryParams } from "ng-zorro-antd/table";
 import { ExecuteWorkflowService } from "../../../service/execute-workflow/execute-workflow.service";
-import { ResultPanelToggleService } from "../../../service/result-panel-toggle/result-panel-toggle.service";
 import { WorkflowActionService } from "../../../service/workflow-graph/model/workflow-action.service";
-import { DEFAULT_PAGE_SIZE, WorkflowResultService } from "../../../service/workflow-result/workflow-result.service";
+import { WorkflowResultService } from "../../../service/workflow-result/workflow-result.service";
+import { PanelResizeService } from "../../../service/workflow-result/panel-resize/panel-resize.service";
 import { isWebPaginationUpdate } from "../../../types/execute-workflow.interface";
 import { IndexableObject, TableColumn } from "../../../types/result-table.interface";
 import { RowModalComponent } from "../result-panel-modal.component";
@@ -46,14 +46,14 @@ export class ResultTableFrameComponent implements OnInit, OnChanges {
   // this starts from **ONE**, not zero
   currentPageIndex: number = 1;
   totalNumTuples: number = 0;
-  pageSize = DEFAULT_PAGE_SIZE;
+  pageSize = 5;
 
   constructor(
     private executeWorkflowService: ExecuteWorkflowService,
     private modalService: NzModalService,
-    private resultPanelToggleService: ResultPanelToggleService,
     private workflowActionService: WorkflowActionService,
-    private workflowResultService: WorkflowResultService
+    private workflowResultService: WorkflowResultService,
+    private resizeService: PanelResizeService
   ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -94,6 +94,20 @@ export class ResultTableFrameComponent implements OnInit, OnChanges {
           this.changePaginatedResultData();
         }
       });
+    this.resizeService.currentSize.pipe(untilDestroyed(this)).subscribe(size => {
+      this.adjustPageSizeBasedOnPanelSize(size.height);
+      let currentPageNum: number = Math.ceil(this.totalNumTuples / this.pageSize);
+      while (this.currentPageIndex > currentPageNum) {
+        this.currentPageIndex -= 1;
+      }
+    });
+  }
+
+  private adjustPageSizeBasedOnPanelSize(panelHeight: number) {
+    const rowHeight = 35;
+    let extra: number = Math.floor((panelHeight - 200) / rowHeight);
+    this.pageSize = 1 + extra;
+    this.resizeService.pageSize = this.pageSize;
   }
 
   /**
@@ -181,7 +195,7 @@ export class ResultTableFrameComponent implements OnInit, OnChanges {
     }
     this.isLoadingResult = true;
     paginatedResultService
-      .selectPage(this.currentPageIndex, DEFAULT_PAGE_SIZE)
+      .selectPage(this.currentPageIndex, this.pageSize)
       .pipe(untilDestroyed(this))
       .subscribe(pageData => {
         if (this.currentPageIndex === pageData.pageIndex) {

core/gui/src/app/app.module.ts
@@ -51,7 +51,6 @@ import { OperatorLabelComponent } from "./workspace/component/left-panel/operato
 import { OperatorMenuComponent } from "./workspace/component/left-panel/operator-menu/operator-menu.component";
 import { PropertyEditorComponent } from "./workspace/component/property-editor/property-editor.component";
 import { TypeCastingDisplayComponent } from "./workspace/component/property-editor/typecasting-display/type-casting-display.component";
-import { ResultPanelToggleComponent } from "./workspace/component/result-panel-toggle/result-panel-toggle.component";
 import { ResultPanelComponent } from "./workspace/component/result-panel/result-panel.component";
 import { VisualizationFrameContentComponent } from "./workspace/component/visualization-panel-content/visualization-frame-content.component";
 import { VisualizationFrameComponent } from "./workspace/component/result-panel/visualization-frame/visualization-frame.component";
@@ -163,7 +162,6 @@ registerLocaleData(en);
     RowModalComponent,
     OperatorLabelComponent,
     MiniMapComponent,
-    ResultPanelToggleComponent,
     ArrayTypeComponent,
     ObjectTypeComponent,
     PresetWrapperComponent,

core/gui/src/app/workspace/component/left-panel/left-panel.component.ts
@@ -70,8 +70,13 @@ export class LeftPanelComponent implements OnDestroy, OnInit {
   }
 
   openFrame(i: number) {
-    if (!i) this.width = 0;
-    else if (!this.width) this.width = 230;
+    if (!i) {
+      this.width = 0;
+      this.height = 65;
+    } else if (!this.width) {
+      this.width = 230;
+      this.height = 300;
+    }
     this.title = this.items[i].title;
     this.currentComponent = this.items[i].component;
     this.currentIndex = i;

core/gui/src/app/workspace/component/property-editor/property-editor.component.html
@@ -18,7 +18,7 @@
     [ngClass]="{'shadow':  !width}">
     <li
       nz-menu-item
-      (click)="width = 0;"
+      (click)="width = 0; height = 65;"
       *ngIf="width">
       <span
         nz-icon
@@ -38,8 +38,9 @@
       id="divider"></li>
     <li
       nz-menu-item
-      (click)="width = 280;"
-      *ngIf="!width">
+      (click)="width = 280; height = 300"
+      *ngIf="!width"
+      nz-tooltip="Property Editor">
       <span
         nz-icon
         nzType="form"></span>

core/gui/src/app/workspace/component/result-panel-toggle/result-panel-toggle.component.html
@@ -1,9 +1,0 @@
-<div
-  class="texera-result-panel-toggle"
-  (click)="onClickResultBar()">
-  <i
-    class="toggle-icon"
-    nz-icon
-    nzTheme="outline"
-    nzType="{{ showResultPanel ? 'down' : 'up' }}"></i>
-</div>

core/gui/src/app/workspace/component/result-panel-toggle/result-panel-toggle.component.scss
@@ -1,21 +1,0 @@
-@import "../workspace.component.scss";
-
-.texera-result-panel-toggle {
-  background: #6c747e;
-  color: white;
-  padding: 2.5px;
-  font-size: 18px;
-  min-height: 100%;
-  font-style: inherit;
-  text-align: center;
-
-  .toggle-icon {
-    display: block;
-    margin: auto;
-  }
-
-  &:hover {
-    background-color: #32363a;
-    cursor: pointer;
-  }
-}

core/gui/src/app/workspace/component/result-panel-toggle/result-panel-toggle.component.spec.ts
@@ -1,25 +1,0 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { ResultPanelToggleComponent } from "./result-panel-toggle.component";
-import { ResultPanelToggleService } from "../../service/result-panel-toggle/result-panel-toggle.service";
-
-describe("ResultPanelToggleComponent", () => {
-  let component: ResultPanelToggleComponent;
-  let fixture: ComponentFixture<ResultPanelToggleComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ResultPanelToggleComponent],
-      providers: [ResultPanelToggleService],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResultPanelToggleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
-});

core/gui/src/app/workspace/component/result-panel-toggle/result-panel-toggle.component.ts
@@ -1,35 +1,0 @@
-import { Component } from "@angular/core";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { ResultPanelToggleService } from "../../service/result-panel-toggle/result-panel-toggle.service";
-
-/**
- * ResultPanelToggleComponent is the small bar directly above ResultPanelComponent at the
- *  bottom level. When the user interface first initialized, ResultPanelComponent will be
- *  hidden and ResultPanelToggleComponent will be at the bottom of the UI.
- *
- * This Component is a toggle button to open / close the result panel.
- */
-@UntilDestroy()
-@Component({
-  selector: "texera-result-panel-toggle",
-  templateUrl: "./result-panel-toggle.component.html",
-  styleUrls: ["./result-panel-toggle.component.scss"],
-})
-export class ResultPanelToggleComponent {
-  public showResultPanel: boolean = false;
-
-  constructor(private resultPanelToggleService: ResultPanelToggleService) {
-    this.resultPanelToggleService
-      .getToggleChangeStream()
-      .pipe(untilDestroyed(this))
-      .subscribe(newPanelStatus => (this.showResultPanel = newPanelStatus));
-  }
-
-  /**
-   * When the result panel toggle is clicked, it will call 'toggleResultPanel'
-   *  to switch the status of the result panel.
-   */
-  onClickResultBar(): void {
-    this.resultPanelToggleService.toggleResultPanel();
-  }
-}

commit 3056cba389c3440a4232a4da517315d65e05e6f1

Fix Result Panel Not Showing Results Occasionally (#2623)
This PR fixes an issue of result panel that could cause results not
being shown from time to time (#2612 ). The issue is caused by #2604.
The `currentPageIndex` should start from 1 but #2604 introduced a logic
that could change the index to 0. Adding a check in the condition could
solve this issue. I tested and did not see problems anymore.

core/gui/src/app/workspace/component/result-panel/result-table-frame/result-table-frame.component.ts
@@ -97,7 +97,7 @@ export class ResultTableFrameComponent implements OnInit, OnChanges {
     this.resizeService.currentSize.pipe(untilDestroyed(this)).subscribe(size => {
       this.adjustPageSizeBasedOnPanelSize(size.height);
       let currentPageNum: number = Math.ceil(this.totalNumTuples / this.pageSize);
-      while (this.currentPageIndex > currentPageNum) {
+      while (this.currentPageIndex > currentPageNum && this.currentPageIndex > 1) {
         this.currentPageIndex -= 1;
       }
     });
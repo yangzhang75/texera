yangzhang75/texera

commit ec01c726cd9c3714610ac6e83d9959ccd53b96e0

Migrate scala control messages to protobuf (#2950)
This PR migrates all Scala control messages and their corresponding handlers to use gRPC.

---

### Main Changes (For both Scala and Python Engine)

1. **Rewrote `AsyncRPCServer` and `AsyncRPCClient`** 
   - Adjusted both to fit into the generated gRPC interface, while keeping RPCs sent and received through actor messages.
   
2. **Centralized RPC Definitions** 
   - All RPC messages and method definitions are now located in: 
     ```
     src/main/protobuf/edu/uci/ics/amber/engine/architecture/rpc
     ```
---

### Steps to Create New RPCs

1. **Create a request proto message** in `controlcommands.proto` with all necessary inputs.
2. **Create a response proto message** in `controlreturns.proto`.
3. **Define the RPC**:
   - For **controller-handled RPCs**, add the definition to `controllerservice.proto`.
   - For **worker-handled RPCs**, add it to `workerservice.proto`.
4. **Generate Scala code** by running `protocGenerate` in the SBT shell.
5. **Generate Python code** by running `./scripts/python-proto-gen.sh` from `core` folder.
6. **Implement the RPC handler**:
    - Scala Engine:  in either `ControllerAsyncRPCHandlerInitializer` or `WorkerAsyncRPCHandlerInitializer`, and move it to the `promisehandlers` folder for better organization.
    - Python Engine: create a new handler under `python/core/architecture/handlers/control` and override corresponding `async` method for the new rpc defined in worker service.

---

### Changes in Sending RPCs (Scala Engine)

Example: Using the `StartWorkflow` RPC, which is handled by the controller.

- **Before this PR:**

  ```scala
  send(StartWorkflow(), CONTROLLER).map { ret =>
    // handle the return value
  }
  ```
- **After this PR:**

  ```scala
  controllerInterface.startWorkflow(EmptyRequest(), mkContext(CONTROLLER)).map { resp =>
      // handle the response
  }
   ```

core/amber/src/main/scala/edu/uci/ics/texera/web/service/ExecutionResultService.scala
@@ -4,10 +4,13 @@ import akka.actor.Cancellable
 import com.fasterxml.jackson.annotation.{JsonTypeInfo, JsonTypeName}
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.typesafe.scalalogging.LazyLogging
-import edu.uci.ics.amber.engine.architecture.controller.ControllerEvent.{
-  ExecutionStateUpdate,
-  FatalError
+import edu.uci.ics.amber.engine.architecture.controller.{ExecutionStateUpdate, FatalError}
+import edu.uci.ics.amber.engine.architecture.rpc.controlreturns.WorkflowAggregatedState.{
+  COMPLETED,
+  FAILED,
+  KILLED
 }
+import edu.uci.ics.amber.engine.architecture.worker.statistics.WorkerState.RUNNING
 import edu.uci.ics.amber.engine.common.{AmberConfig, AmberRuntime, IncrementalOutputMode}
 import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.amber.engine.common.model.tuple.Tuple
@@ -27,12 +30,6 @@ import edu.uci.ics.texera.web.storage.{
   WorkflowStateStore
 }
 import edu.uci.ics.amber.engine.common.workflowruntimestate.ExecutionMetadataStore
-import edu.uci.ics.amber.engine.common.workflowruntimestate.WorkflowAggregatedState.{
-  COMPLETED,
-  FAILED,
-  KILLED,
-  RUNNING
-}
 import edu.uci.ics.texera.web.SubscriptionManager
 import edu.uci.ics.texera.workflow.common.storage.OpResultStorage
 import edu.uci.ics.texera.workflow.common.workflow.LogicalPlan

commit 975ec0d9818383fe699da1f8489b8ffd01c2ee1d

Fix nullpointer exception on result panel (#2986)
This bug is caused by a wrong import of the `RUNNING` state introduced
in #2950.

Co-authored-by: Xinyuan Lin <xinyual3@uci.edu>

core/amber/src/main/scala/edu/uci/ics/texera/web/service/ExecutionResultService.scala
@@ -8,9 +8,9 @@ import edu.uci.ics.amber.engine.architecture.controller.{ExecutionStateUpdate, F
 import edu.uci.ics.amber.engine.architecture.rpc.controlreturns.WorkflowAggregatedState.{
   COMPLETED,
   FAILED,
+  RUNNING,
   KILLED
 }
-import edu.uci.ics.amber.engine.architecture.worker.statistics.WorkerState.RUNNING
 import edu.uci.ics.amber.engine.common.{AmberConfig, AmberRuntime, IncrementalOutputMode}
 import edu.uci.ics.amber.engine.common.client.AmberClient
 import edu.uci.ics.amber.engine.common.model.tuple.Tuple